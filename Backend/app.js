15432f4e7812eea3310e653cff12334dd06ad63e
NoorElDeen153 <hodaroshdy106@gmail.com>
2022-08-24 20:13:48 +0200

    update

--- Backend/app.js
+++ Backend/app.js
@@ -4,22 +4,11 @@ const router = require('./modules/routes/main.routes')
 const path = require('path');
 const cors = require('cors')
 const app = express()
-
-
-
 app.use(express.json())
-
 connection()
-
-
 app.use(express.static('./dist/portfolio'));
-
-
-
 app.use(cors());
-
 app.use(router)
-
 app.listen(3000,()=>{
 console.log("hello to back");
 })
--- Backend/modules/routes/main.routes.js
+++ Backend/modules/routes/main.routes.js
@@ -1,5 +1,4 @@
 const { login, register } = require('../controller/main.controller')
-console.log("object1");
 const router = require('express').Router()
 router.get('/*', (req, res) => res.sendFile('index.html', {root: './dist/portfolio'}));
 router.post('/moviedb/login',login)
